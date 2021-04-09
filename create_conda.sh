63e44ef1e0b4e8859ec347dd3f89cf95a4b824df
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-04-09 18:17:16 +0200

    fix ci

--- create_conda.sh
+++ create_conda.sh
@@ -1,15 +1,6 @@
-pythonversion=3.8
-nglviewversion=2.7.7
-labversion=2.1.5
-ipywidgetsversion=7.5.1
-
-conda create -n moffragmentor python=$pythonversion -y
-source activate moffragmentor
-conda install ipywidgets==$ipywidgetsversion -c conda-forge -y
-conda install nglview==$nglviewversion -c conda-forge -y
-conda install jupyterlab=$labversion  -y -c conda-forge
+conda create -n lab python=3.8 -y
+source activate lab
+conda install jupyterlab=3 nglview -c conda-forge -y
 conda install openbabel rdkit ase click
 pip install "pymatgen>=2021.1,<2022"
 pip install git+https://github.com/skearnes/rdkit-utils.git
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install nglview-js-widgets@$nglviewversion
