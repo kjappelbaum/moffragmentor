282c8b3058c3f75183fa0955785ffc845d5707ab
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-04-09 18:32:14 +0200

    fix ci

--- create_conda.sh
+++ create_conda.sh
@@ -1,6 +1,6 @@
 conda create -n lab python=3.8 -y
 source activate lab
 conda install jupyterlab=3 nglview -c conda-forge -y
-conda install openbabel rdkit ase click
+conda install openbabel rdkit ase click -c conda-forge
 pip install "pymatgen>=2021.1,<2022"
 pip install git+https://github.com/skearnes/rdkit-utils.git
